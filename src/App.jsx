6db3b1f8ca808dea4612c1faa20d5c555eca2793
Gil Klein <kleinster@gmail.com>
2024-12-05 19:22:41 -0500

    Add GitHub Pages deployment workflow

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import IrregularVerbsTable from './components/IrregularVerbsTable'
+import './App.css'
+
+function App() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <IrregularVerbsTable />
+    </div>
+  )
+}
+
+export default App
